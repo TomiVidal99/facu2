fe31f21074967335fee458ff92a78637f739f8a7
Tomás Vidal <tomi_vidal_99@hotmail.com>
2024-04-27 13:51:41 -0300

    Trabajando en el 5

---
+++ Facu/Arquitectura de computadores/TP 1/programas_tp_1/5.asm
@@ -0,0 +1,7 @@
+/ Problema 4. Practica 1 Arquitectura de Computadoras
+/ Nombre: Tomás Vidal (69854/4)
+/ Se implementa la multiplicación de dos números: num = x*y
+/ se agrega 'x' a 'num' 'y' veces
+
+/ Comienza el programa
+ORG 100
